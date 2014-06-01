23b3d7f694b21289bc3943774220a0ea5ecacd39
Kevin Lohman <badpirate@gmail.com>
2014-06-01 15:14:21 -0700

    Ditch the default failed unit test.  I won’t be likely to use these.

--- hearthstats/hearthstatsTests/hearthstatsTests.m
+++ hearthstats/hearthstatsTests/hearthstatsTests.m
@@ -25,10 +25,4 @@
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
-
 @end
