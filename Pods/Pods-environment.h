09f8d1904d2a07984bbb2674acec8f41e691d8fd
Muntrue <Mario@Marios-MacBook-Pro.local>
2014-09-12 15:32:17 +0200

    Updated pods
    pod update
    Update all pods
    Analyzing dependencies
    Downloading dependencies
    Installing AFNetworking 2.4.1 (was 2.3.1)
    Installing CrashlyticsFramework 2.2.4 (was 2.2.1)
    Installing Harpy 2.6.0 (was 2.5.4)
    Installing KIF 3.0.8 (was 3.0.6)
    Installing Mixpanel 2.5.2 (was 2.4.0)
    Using SSKeychain (1.2.2)
    Installing SVProgressHUD HEAD based on 1.0 (was HEAD based on 1.0)
    Using UIColor-ColorWithHexAndAlpha (1.2.0)
    Using UIDevice-SupportedDevices (1.0.1)
    Installing UIFloatLabelTextField 1.0.14 (was 1.0.7)
    Installing UIFloatLabelTextView 1.0.7 (was 1.0.1)
    Using UIView-AutoLayoutView (1.0.0)
    Generating Pods project
    Integrating client project

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -9,62 +9,62 @@
 // AFNetworking
 #define COCOAPODS_POD_AVAILABLE_AFNetworking
 #define COCOAPODS_VERSION_MAJOR_AFNetworking 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_MINOR_AFNetworking 4
 #define COCOAPODS_VERSION_PATCH_AFNetworking 1
 
 // AFNetworking/NSURLConnection
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLConnection
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLConnection 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLConnection 3
+#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLConnection 4
 #define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLConnection 1
 
 // AFNetworking/NSURLSession
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLSession
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLSession 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLSession 3
+#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLSession 4
 #define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLSession 1
 
 // AFNetworking/Reachability
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_Reachability
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_Reachability 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_Reachability 3
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Reachability 4
 #define COCOAPODS_VERSION_PATCH_AFNetworking_Reachability 1
 
 // AFNetworking/Security
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_Security
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_Security 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_Security 3
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Security 4
 #define COCOAPODS_VERSION_PATCH_AFNetworking_Security 1
 
 // AFNetworking/Serialization
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_Serialization
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_Serialization 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_Serialization 3
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Serialization 4
 #define COCOAPODS_VERSION_PATCH_AFNetworking_Serialization 1
 
 // AFNetworking/UIKit
 #define COCOAPODS_POD_AVAILABLE_AFNetworking_UIKit
 #define COCOAPODS_VERSION_MAJOR_AFNetworking_UIKit 2
-#define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 3
+#define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 4
 #define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 1
 
 // CrashlyticsFramework
 #define COCOAPODS_POD_AVAILABLE_CrashlyticsFramework
 #define COCOAPODS_VERSION_MAJOR_CrashlyticsFramework 2
 #define COCOAPODS_VERSION_MINOR_CrashlyticsFramework 2
-#define COCOAPODS_VERSION_PATCH_CrashlyticsFramework 1
+#define COCOAPODS_VERSION_PATCH_CrashlyticsFramework 4
 
 // Harpy
 #define COCOAPODS_POD_AVAILABLE_Harpy
 #define COCOAPODS_VERSION_MAJOR_Harpy 2
-#define COCOAPODS_VERSION_MINOR_Harpy 5
-#define COCOAPODS_VERSION_PATCH_Harpy 4
+#define COCOAPODS_VERSION_MINOR_Harpy 6
+#define COCOAPODS_VERSION_PATCH_Harpy 0
 
 // Mixpanel
 #define COCOAPODS_POD_AVAILABLE_Mixpanel
 #define COCOAPODS_VERSION_MAJOR_Mixpanel 2
-#define COCOAPODS_VERSION_MINOR_Mixpanel 4
-#define COCOAPODS_VERSION_PATCH_Mixpanel 0
+#define COCOAPODS_VERSION_MINOR_Mixpanel 5
+#define COCOAPODS_VERSION_PATCH_Mixpanel 2
 
 // SSKeychain
 #define COCOAPODS_POD_AVAILABLE_SSKeychain
@@ -94,13 +94,13 @@
 #define COCOAPODS_POD_AVAILABLE_UIFloatLabelTextField
 #define COCOAPODS_VERSION_MAJOR_UIFloatLabelTextField 1
 #define COCOAPODS_VERSION_MINOR_UIFloatLabelTextField 0
-#define COCOAPODS_VERSION_PATCH_UIFloatLabelTextField 7
+#define COCOAPODS_VERSION_PATCH_UIFloatLabelTextField 14
 
 // UIFloatLabelTextView
 #define COCOAPODS_POD_AVAILABLE_UIFloatLabelTextView
 #define COCOAPODS_VERSION_MAJOR_UIFloatLabelTextView 1
 #define COCOAPODS_VERSION_MINOR_UIFloatLabelTextView 0
-#define COCOAPODS_VERSION_PATCH_UIFloatLabelTextView 1
+#define COCOAPODS_VERSION_PATCH_UIFloatLabelTextView 7
 
 // UIView-AutoLayoutView
 #define COCOAPODS_POD_AVAILABLE_UIView_AutoLayoutView
